565c8c371241145e1cfecf0f8210ea5345bc0981
nathan-assefa <nnathanassefa@gmail.com>
2023-01-21 03:43:00 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		state = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 2 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{

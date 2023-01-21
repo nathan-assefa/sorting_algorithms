9b8c46febcf8bec774e190d86465ae1468201439
nathan-assefa <nnathanassefa@gmail.com>
2023-01-21 05:21:02 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,36 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending order using Bubble
- * sort algorithm.
- *
- * @array: an array of integers
- * @size: size of the array
- *
- * Return: void.
+ * bubble_sort - Used to sort a given set of n elements
+ * provided in form of an array with n number of elements.
+ * Bubble Sort compares all the element one by one and sort
+ * them based on their values.
+ * @array: Array of data to be sorted
+ * @size: Size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int temp = 0, flag = 1;
+	int i = 0, j = 0, sorted = 1, tmp = 0;
 
-	if (!array || !size)
+	if (size < 2)
 		return;
-
-	while (flag)
+	j = size;
+	while (sorted == 1 && j > 0)
 	{
-		flag = 0;
-		for (i = 0; i < size - 1; i++)
+		sorted = 0;
+		i = 1;
+		while (i <= j)
 		{
-			if (array[i] > array[i + 1])
+			if (array[i - 1] > array[i] && array[i] != '\0')
 			{
-				temp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = temp;
-				flag = 1;
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
 				print_array(array, size);
+				sorted = 1;
 			}
+			i += 1;
 		}
+		j -= 1;
 	}
 }

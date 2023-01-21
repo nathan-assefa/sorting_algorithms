9a0f916b785ac81de13b261f66760924bcd6ced0
nathan-assefa <nnathanassefa@gmail.com>
2023-01-21 12:27:51 -0800

    sorting_algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,72 +1,76 @@
 #include "sort.h"
 
 /**
- * * _swap- to swap two numbers
- * * @array: array to be swapped
- * * @size: size of array
- * * @a: The first number to be swapped
- * * @b: The secong number to be swapped
+ * swap - swaps 2 int values
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @a: address of first value
+ * @b: address of second value
+ *
+ * Return: void
  */
-void _swap(int array[], int size, int *a, int *b)
+void swap(int *array, size_t size, int *a, int *b)
 {
 	if (*a != *b)
 	{
-		int tmp = *a;
-		*a = *b;
-		*b = tmp;
-		print_array(array, size);
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
 	}
 }
 
 /**
- * * partitioned- To divide an array and merge
- * * @array: an array to be divided and merged
- * * @low: 1st index in the array
- * * @high: last index in the array
- * * @size: size of an array
- * * Return: pivot point
+ * lomuto_partition - partitions the array
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
  */
-int partitioned(int array[], int low, int high, int size)
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
 {
-	int i, j, pivot_value = array[high];
+	int i, j, pivot = array[hi];
 
-	for (i = low, j = low; j < high; j++)
-	{
-		if (array[j] < pivot_value)
-		{
-			_swap(array, size, &array[j], &array[i++]);
-		}
-	}
-	_swap(array, size, &array[i], &array[high]);
+	for (i = j = lo; j < hi; j++)
+		if (array[j] < pivot)
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[hi]);
+
 	return (i);
 }
 
 /**
- * * quick_sort_recursion- recursive function to sort an array
- * * @arr: an array to be sorted
- * * @low: 1st element in the array
- * * @high: last element in the array
- * * @size: size of an array
+ * quicksort - quicksorts via Lomuto partitioning scheme
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
  */
-void quick_sort_recursion(int *arr, int low, int high, int size)
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
 {
-	int pivot_idx;
-
-	if (low < high)
+	if (lo < hi)
 	{
-		pivot_idx = partitioned(arr, low, high, size);
-		quick_sort_recursion(arr, low, pivot_idx - 1, size);
-		quick_sort_recursion(arr, pivot_idx + 1, high, size);
+		size_t p = lomuto_partition(array, size, lo, hi);
+
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
 	}
 }
 
 /**
- * * quick_sort- To sort an array using quick sort
- * * @array: array to be sorted
- * * @size: size of an array
+ * quick_sort - calls quicksort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array && size >= 2)
-		quick_sort_recursion(array, 0, size - 1, size);
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
 }
---
+++ quick_test.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+/**
+ * * _swap- to swap two numbers
+ * * @array: array to be swapped
+ * * @size: size of array
+ * * @a: The first number to be swapped
+ * * @b: The secong number to be swapped
+ */
+void _swap(int array[], int size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		int tmp = *a;
+		*a = *b;
+		*b = tmp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * * partitioned- To divide an array and merge
+ * * @array: an array to be divided and merged
+ * * @low: 1st index in the array
+ * * @high: last index in the array
+ * * @size: size of an array
+ * * Return: pivot point
+ */
+int partitioned(int array[], int low, int high, int size)
+{
+	int i, j, pivot_value = array[high];
+
+	for (i = low, j = low; j < high; j++)
+	{
+		if (array[j] < pivot_value)
+		{
+			_swap(array, size, &array[j], &array[i++]);
+		}
+	}
+	_swap(array, size, &array[i], &array[high]);
+	return (i);
+}
+
+/**
+ * * quick_sort_recursion- recursive function to sort an array
+ * * @arr: an array to be sorted
+ * * @low: 1st element in the array
+ * * @high: last element in the array
+ * * @size: size of an array
+ */
+void quick_sort_recursion(int *arr, int low, int high, int size)
+{
+	int pivot_idx;
+
+	if (low < high)
+	{
+		pivot_idx = partitioned(arr, low, high, size);
+		quick_sort_recursion(arr, low, pivot_idx - 1, size);
+		quick_sort_recursion(arr, pivot_idx + 1, high, size);
+	}
+}
+
+/**
+ * * quick_sort- To sort an array using quick sort
+ * * @array: array to be sorted
+ * * @size: size of an array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array && size >= 2)
+		quick_sort_recursion(array, 0, size - 1, size);
+}

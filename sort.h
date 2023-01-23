8de8a1578e52febc23ccb0bacfc910739a075fc8
nathan-assefa <nnathanassefa@gmail.com>
2023-01-23 01:35:14 -0800

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 #endif /** SORT_H **/

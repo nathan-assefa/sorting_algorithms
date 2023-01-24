430b5cbf94f3a5d6908b2270825d517b7da0ef7d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-23 23:17:42 -0800

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,28 +4,28 @@
  * @list: Doubly linked list with nodes to sort acording to number n.
  * @aux: auxiliar node to compare
  */
-void swap_head(listint_t **list, listint_t *aux)
+void swap_head(listint_t **list, listint_t *tmp)
 {
-	aux->prev->next = aux->next;
-	if (aux->next)
-		aux->next->prev = aux->prev;
-	aux->next = aux->prev;
-	aux->prev = aux->prev->prev;
-	aux->next->prev = aux;
-	*list = aux;
+	(*list)->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = *list;
+	(*list)->prev = tmp;
+	tmp->next = *list;
+	tmp->prev = NULL;
+	*list = tmp;
 }
 /**
  * swap_middle - swaps a node at the middle of the list
  * @aux: auxiliar node to compare
  */
-void swap_middle(listint_t *aux)
+void swap_middle(listint_t *tmp)
 {
-	aux->prev->next = aux->next;
-	aux->next->prev = aux->prev;
-	aux->prev->prev->next = aux;
-	aux->next = aux->prev;
-	aux->prev = aux->next->prev;
-	aux->next->prev = aux;
+	tmp->prev->next = tmp->next;
+	tmp->next->prev = tmp->prev;
+	tmp->prev->prev->next = tmp;
+	tmp->next = tmp->prev;
+	tmp->prev = tmp->next->prev;
+	tmp->next->prev = tmp;
 }
 /**
  * swap_tail - swaps a node at the end of the list

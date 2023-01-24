6a7b5e708006a7ad8c8c5eb5a3e98ac97c8e438b
nathan-assefa <nnathanassefa@gmail.com>
2023-01-23 23:29:10 -0800

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,83 +1,70 @@
 #include "sort.h"
+
 /**
- * swap_head - swaps a node at the beggining of the list
- * @list: Doubly linked list with nodes to sort acording to number n.
- * @tmp: auxiliar node to compare
+ * * _swap- To swap elements of a list
+ * * @list: The main list to be swapped
+ * * @tmp: tmporary pointer to list
  */
-void swap_head(listint_t **list, listint_t *tmp)
-{
-	(*list)->next = tmp->next;
-	if (tmp->next)
-		tmp->next->prev = *list;
-	(*list)->prev = tmp;
-	tmp->next = *list;
-	tmp->prev = NULL;
-	*list = tmp;
-}
-/**
- * swap_middle - swaps a node at the middle of the list
- * @tmp: auxiliar node to compare
- */
-void swap_middle(listint_t *tmp)
-{
-	tmp->prev->next = tmp->next;
-	tmp->next->prev = tmp->prev;
-	tmp->prev->prev->next = tmp;
-	tmp->next = tmp->prev;
-	tmp->prev = tmp->next->prev;
-	tmp->next->prev = tmp;
-}
-/**
- * swap_tail - swaps a node at the end of the list
- * @tmp: auxiliar node to compare
- */
-void swap_tail(listint_t *tmp)
+void _swap(listint_t **list, listint_t *tmp)
 {
 	listint_t *tmp2;
 
-	tmp2 = tmp->prev;
-	tmp2->prev->next = tmp;
-	tmp2->next = tmp->next;
-	tmp->prev = tmp2->prev;
-	tmp2->prev = tmp;
-	tmp->next = tmp2;
+	if (!tmp->prev->prev)
+	{
+		(*list)->next = tmp->next;
+		if (tmp->next)
+			tmp->next->prev = *list;
+		(*list)->prev = tmp;
+		tmp->next = *list;
+		tmp->prev = NULL;
+		*list = tmp;
+	}
+
+	else if (tmp->prev->prev && tmp->next)
+	{
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		tmp->prev->prev->next = tmp;
+		tmp->next = tmp->prev;
+		tmp->prev = tmp->next->prev;
+		tmp->next->prev = tmp;
+
+	}
+	else if (!tmp-next)
+	{
+		tmp2 = tmp->prev;
+		tmp2->prev->next = tmp;
+		tmp2->next = tmp->next;
+		tmp->prev = tmp2->prev;
+		tmp2->prev = tmp;
+		tmp->next = tmp2;
+	}
 }
+
 /**
- * insertion_sort_list - Insertion sort is a simple sorting algorithm
- * that builds the final sorted array (or list) one item at a time.
- * @list: Doubly linked list with nodes to sort acording to number n.
+ * * insertion_sort_list- Insertion sorting
+ * * @list: The list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *aux = NULL;
-	int i = 0, j = 0;
+	listint_t *tmp, *head = (*list)->next;
 
-	if (!list || !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	aux = (*list)->next;
-	while (aux)
+
+	for (; head; head = head->next)
 	{
-		i++;
-		while (aux->prev)
+		tmp = head;
+
+		while (tmp->prev)
 		{
-			if (aux->prev->n > aux->n)
+			if (tmp->prev->n > tmp->n)
 			{
-				if (!aux->prev->prev)
-					swap_head(list, aux);
-				else if (aux->prev->prev && aux->next)
-					swap_middle(aux);
-				else if (!aux->next)
-					swap_tail(aux);
-				print_list(*list);
+				_swap(list, tmp);
+				print_list((const listint_t *)*list);
 			}
 			else
-				aux = aux->prev;
+				tmp = tmp->prev;
 		}
-		while (j <= i)
-		{
-			aux = aux->next;
-			j++;
-		}
-		j = 0;
 	}
 }
